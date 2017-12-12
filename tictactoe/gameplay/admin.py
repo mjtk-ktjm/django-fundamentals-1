c258c6e6cc5bd2603d5269805f0768abed7d6005
Mathew Kelly <mjtk.ktjm@gmail.com>
2017-12-12 17:51:08 -0500

    Tweak admin display for gameplay models.

--- tictactoe/gameplay/admin.py
+++ tictactoe/gameplay/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import Game, Move
 
-admin.site.register(Game)
-admin.site.register(Move)
 
+#class GameAdmin(admin.ModelAdmin):
+#   list_display = ['first_player', 'second_player', 'status']
+#admin.site.register(Game, GameAdmin)
+# or...
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+  list_display = ('id', 'first_player', 'second_player', 'status')
+  list_editable = ('status',)
+
+
+admin.site.register(Move)
--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,15 +1,33 @@
+# from __future__ import unicode_literals
+# from django.utils.encoding import python2_unicode_compatible
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
+GAME_STATUS_CHOICES = (
+  ('F', 'First Player to Move'),
+  ('S', 'Second Player to Move'),
+  ('W', 'First Player Wins'),
+  ('L', 'Second Player Wins'),
+  ('D', 'Draw'),
+)
+
+# @python_2_unicode_compatible
 class Game(models.Model):
   first_player = models.ForeignKey(User,
-                                  related_name="games_first_player")
+    related_name="games_first_player")
   second_player = models.ForeignKey(User,
-                                  related_name="games_second_player")
+    related_name="games_second_player")
   start_time = models.DateTimeField(auto_now_add=True)
   last_active = models.DateTimeField(auto_now=True)
-  status = models.CharField(max_length=1, default='F')
+  status = models.CharField(max_length=1, 
+    default='F', 
+    choices=GAME_STATUS_CHOICES)
+
+  def __str__(self):
+    return "{} vs {}".format(self.first_player, self.second_player)
+
 
 class Move(models.Model):
   x = models.IntegerField()
